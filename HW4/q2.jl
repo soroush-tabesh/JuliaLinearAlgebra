f4d20981a5d5bdda0ae1ce8cd089e0721e064c5b
Soroush Tabesh <soroushtabesh@gmail.com>
2021-04-14 00:19:58 +0430

    Use decomposition to find limit

--- HW4/q2.jl
+++ HW4/q2.jl
@@ -21,7 +21,7 @@ P = Matrix(hcat(P...)')
 last_P = I
 d = zeros(Int32, n)
 eps = 1e-12
-for t = 1:(2*n+2)
+for t = 1:(n+2)
     global last_P = last_P * P
     for i = 1:n
         if last_P[i, i] > eps
@@ -31,22 +31,22 @@ for t = 1:(2*n+2)
 end
 
 if d == ones(Int32, n)
-    # egi = real(transpose(inv(eigvecs(P))))
-    # egv = real(eigvals(P))
-    # b = egi \ x
-    # for i = 1:(size(egv)[1])
-    #     if egv[i] == egv[i]' && abs(egv[i] - 1) > eps
-    #         b[i] = 0
-    #     end
-    # end
-    # xf = egi * b
-    xf = copy(x)'
-    while true
-        global xf = xf*P
-        if norm(xf - xf*P) < 1e-8
-            break
+    egi = inv(eigvecs(P))'
+    egv = eigvals(P)
+    b = egi \ x
+    for i = 1:(size(egv)[1])
+        if norm(egv[i] - 1) > eps
+            b[i] = 0
         end
     end
+    xf = egi * b
+    # xf = copy(x)'
+    # while true
+    #     global xf = xf*P
+    #     if norm(xf - xf*P) < 1e-8
+    #         break
+    #     end
+    # end
     for e in xf
         @printf("%.7f\n", real(e))
     end
