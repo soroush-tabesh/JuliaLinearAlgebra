5047e122518286b5fba6af8704fa7c8ad3be4ad2
Soroush Tabesh <soroushtabesh@gmail.com>
2021-06-10 17:03:06 +0430

    Add hw8/q1

---
+++ HW8/q1.jl
@@ -0,0 +1,21 @@
+using LinearAlgebra
+
+m = parse(Int, strip(readline()))
+A = [parse.(Float64, split(readline())) for i = 1:n]
+A = collect(hcat(A...)')
+b = parse.(Float64, split(strip(readline())))
+n = length(b)
+##
+function polytope_volume(A, b, N = 1000000)
+
+    bounds = vec(minimum(b ./ A, dims = 1))
+
+    S = rand(Float64, (n, N))
+    S .*= bounds
+    M = count(sum((A * S) .< b, dims = 1) .== n)
+
+    Ṽ = prod(bounds)
+    V = M / N * Ṽ
+end
+
+polytope_volume(A, b)
