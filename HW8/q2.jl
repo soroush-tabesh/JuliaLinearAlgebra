98a88e04ba47c8f4122ef6f48ecddfe301f0a3bd
Soroush Tabesh <soroushtabesh@gmail.com>
2021-06-13 21:18:42 +0430

    Add gradient descent

--- HW8/q2.jl
+++ HW8/q2.jl
@@ -19,73 +19,40 @@ end
 # intensity = parse(Int, strip(readline()))
 b = ones(50) .* intensity
 
-function BVLS(A, b, l, u, max_iter = 1000, eps = 1e-8)
+function BVLS(A, b, l, u, max_iter = 10000, eps = 1e-8, rate = 0.01)
     m, n = size(A)
-    F = Set([])
-    U = Set([])
-    L = Set(1:n)
+    F = []
+    U = []
+    L = []
     x = ones(n) * l
     iter = 0
-    while length(F) < n && iter < max_iter
+    while iter < max_iter
         iter += 1
 
-        # Kuhn-Tucker convergence test
         w = A' * (b - A * x)
-        if all(w[collect(L)] .<= 0) &&
-           all(w[collect(U)] .>= 0) &&
-           all((w[collect(F).<eps]) & (w[collect(F).>-eps]))
+        w ./= norm(w)
+
+        L = findall((x .< (l + eps)) .& (w .<= 0))
+        U = findall((x .> (u - eps)) .& (w .>= 0))
+        F = setdiff(1:n, union(U, L))
+        # Kuhn-Tucker convergence test
+        if all((w[F] .< eps) .& (w[F] .> -eps))
             break
         end
 
-        # L = findall((x .< (l + eps)) & (w .<= 0))
-        # U = findall((x .> (u - eps)) & (w .>= 0))
-        # F = setdiff(collect(1:n), union(U, L))
-
-        sg = zeros(n)
-        sg[collect(L)] .= 1
-        sg[collect(U)] .= -1
-        ts = findmax(t -> sg[t] * w[t], union(U, L))[2]
-        pop!(L, ts, nothing)
-        pop!(U, ts, nothing)
-        push!(F, ts)
-        println(length(F), " ", ts, " ", w[ts], " ", x[ts])
-
-        B = collect(union(U, L))
-        bp = vec(b - A[:, B] * x[B])
-        Ap = A[:, collect(F)]
-        z = Ap \ bp
-
-        if all(z .> l) && all(z .< u)
-            x[collect(F)] .= z
-            continue
-        end
-
-        α = Inf
-        for jp = 1:length(z)
-            j = collect(F)[jp]
-            if z[jp] > u || z[jp] < l
-                if z[jp] > x[j]
-                    a = (u - x[j]) / (z[jp] - x[j])
-                else
-                    a = (l - x[j]) / (z[jp] - x[j])
-                end
-                α = min(α, a)
+        w[L] .= 0
+        w[U] .= 0
+        a = rate
+        for i in F
+            if w[i] < 0
+                a = min(a, (l - x[i]) / w[i])
+            else
+                a = min(a, (u - x[i]) / w[i])
             end
         end
 
-        Fnew = copy(F)
-        for jp = 1:length(z)
-            j = collect(F)[jp]
-            x[j] += α * (z[jp] - x[j])
-            if x[j] >= u - eps
-                pop!(Fnew, j)
-                push!(U, j)
-            elseif x[j] <= l + eps
-                pop!(Fnew, j)
-                push!(L, j)
-            end
-        end
-        F = Fnew
+        x .+= a * w
+
     end
     return x
 end
@@ -93,4 +60,6 @@ end
 # p = BVLS([1 -2 3; 4 5 -6; 7 8 9; -10 11 12], [15, 16, 10, 18], 0.0, 1.0)
 p = BVLS(A, b, 0.0, 1.0)
 ##
-writedlm("A.txt", A, ',')
+norm(A*p-b)
+##
+# writedlm("A.txt", A, ',')
