3a97ea422efb46950b2d970538cf6324d21a3b9d
Soroush Tabesh <soroushtabesh@gmail.com>
2021-06-13 22:55:17 +0430

    Stable BVLS

--- HW8/q2.jl
+++ HW8/q2.jl
@@ -19,7 +19,7 @@ end
 # intensity = parse(Int, strip(readline()))
 b = ones(50) .* intensity
 
-function BVLS(A, b, l, u, max_iter = 10000, eps = 1e-8, rate = 0.01)
+function BVLS_GD(A, b, l, u, max_iter = 10000, eps = 1e-8, rate = 0.01)
     m, n = size(A)
     F = []
     U = []
@@ -35,7 +35,7 @@ function BVLS(A, b, l, u, max_iter = 10000, eps = 1e-8, rate = 0.01)
         L = findall((x .< (l + eps)) .& (w .<= 0))
         U = findall((x .> (u - eps)) .& (w .>= 0))
         F = setdiff(1:n, union(U, L))
-        # Kuhn-Tucker convergence test
+        # convergence test
         if all((w[F] .< eps) .& (w[F] .> -eps))
             break
         end
@@ -57,9 +57,91 @@ function BVLS(A, b, l, u, max_iter = 10000, eps = 1e-8, rate = 0.01)
     return x
 end
 
-# p = BVLS([1 -2 3; 4 5 -6; 7 8 9; -10 11 12], [15, 16, 10, 18], 0.0, 1.0)
-p = BVLS(A, b, 0.0, 1.0)
+function BVLS(A, b, l, u, max_iter = 2 * size(A)[2], eps = 1e-12)
+    m, n = size(A)
+    F = Set([])
+    U = Set([])
+    L = Set(1:n)
+    x = ones(n) * l
+    iter = 0
+    ign = []
+    while length(F) < n && iter < max_iter
+        iter += 1
+
+        # Kuhn-Tucker convergence test
+        w = A' * (b - A * x)
+        if all(w[collect(L)] .<= 0) &&
+           all(w[collect(U)] .>= 0) &&
+           all((w[collect(F)] .< eps) .& (w[collect(F)] .> -eps))
+            break
+        end
+
+        T = setdiff(union(U, L), ign)
+        if length(T) == 0
+            break
+        end
+
+        sg = zeros(n)
+        sg[collect(L)] .= 1
+        sg[collect(U)] .= -1
+        ts = findmax(t -> sg[t] * w[t], T)[2]
+        pop!(L, ts, nothing)
+        pop!(U, ts, nothing)
+        push!(F, ts)
+        println(length(F), " ", ts, " ", w[ts], " ", x[ts])
+
+        B = collect(union(U, L))
+        bp = vec(b - A[:, B] * x[B])
+        Ap = A[:, collect(F)]
+        z = Ap \ bp
+
+        if all(z .> l) && all(z .< u)
+            x[collect(F)] .= z
+            continue
+        end
+
+        α = Inf
+        for jp = 1:length(z)
+            j = collect(F)[jp]
+            if z[jp] > u || z[jp] < l
+                if z[jp] > x[j]
+                    a = (u - x[j]) / (z[jp] - x[j])
+                else
+                    a = (l - x[j]) / (z[jp] - x[j])
+                end
+                α = min(α, a)
+            end
+        end
+
+        if α < eps
+            push!(ign,ts)
+        else
+            ign = []
+        end
+
+        Fnew = copy(F)
+        for jp = 1:length(z)
+            j = collect(F)[jp]
+            x[j] += α * (z[jp] - x[j])
+            if x[j] >= u - eps
+                pop!(Fnew, j)
+                push!(U, j)
+            elseif x[j] <= l + eps
+                pop!(Fnew, j)
+                push!(L, j)
+            end
+        end
+        F = Fnew
+    end
+    return x
+end
+
 ##
-norm(A*p-b)
+p = BVLS(A, b, 0.0, 1.0)
+norm(A * p - b)
+
+##
+p = BVLS([1 -2 3; 4 5 -6; 7 8 9; -10 11 12], [15, 16, 10, 18], 0.0, 1.0)
+norm([1 -2 3; 4 5 -6; 7 8 9; -10 11 12] * p - [15, 16, 10, 18])
 ##
 # writedlm("A.txt", A, ',')
